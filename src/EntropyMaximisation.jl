2445e87206531483e29c201d68848430afed6885
jakubkislinger <jakub.kislinger@dnai.ai>
2024-01-17 15:53:19 +0100

    :sparkles: Documentation added

--- src/EntropyMaximisation.jl
+++ src/EntropyMaximisation.jl
@@ -30,7 +30,32 @@ module EntropyMaximisation
     include("MatlabParser.jl")
     include("projectedGradient.jl")
 
+    """
+    maximize_entropy(joined_probability::Array{T}, marginal_size; method = Cone())::EMResult where T <: Real 
 
+    Finds distribution that maximizes entropy while having fixed all marginals of size `marginal_size`. Optional
+    argument `method` specifies which method to use for optimization. Default is `Cone()`.
+
+    # Example
+
+    ```julia-repl
+    julia> x = [0.1 0.4; 0.4 0.1]
+    2×2 Matrix{Float64}:
+    0.1  0.4
+    0.4  0.1
+
+    julia> maximize_entropy(x, 2)
+    Entropy: 1.7219280948873623
+    Distribution:
+    [0.1 0.4; 0.4 0.1]
+
+    julia> maximize_entropy(x, 1; method = Ipfp())
+    Progress: 100%|███████████████████████████████████████████████████████████████████████████████████████████████| Time: 0:00:00
+    Entropy: 2.0
+    Distribution:
+    [0.25 0.25; 0.25 0.25]
+    ```
+    """
     function maximize_entropy(joined_probability::Array{T}, marginal_size; method = Cone())::EMResult where T <: Real 
 
         marginal_size > ndims(joined_probability) && 
@@ -39,6 +64,10 @@ module EntropyMaximisation
             throw(DomainError("Marginal size has to be possitive"))
         !isapprox(sum(joined_probability), 1) && 
             throw(DomainError("Joined probability has to sum to 1"))
+
+        if marginal_size == ndims(joined_probability)
+            return EMResult(joined_probability)
+        end
 
         marginals = permutations_of_length(marginal_size, ndims(joined_probability))
 
@@ -53,33 +82,76 @@ module EntropyMaximisation
         end
     end
 
+    """
+    connected_information(joined_probability::Array{T}, order::Int; method = Cone(MosekOptimizer())) where T <: Real
 
-    function connected_information(joined_probability::Array{T}, marginal_size::Int; method = Cone(MosekOptimizer())) where T <: Real
+    Computes connected information for given joined probability and orders `order`. Optional argument `method` 
+    specifies which method to use for optimization. Default is `Cone()`.    
+        
+    # Examples
 
-        marginal_size > ndims(joined_probability) && 
+    ```julia-repl
+    julia> x = [0.1 0.4; 0.4 0.1]
+    2×2 Matrix{Float64}:
+     0.1  0.4
+     0.4  0.1
+
+    julia> connected_information(x, 2; method = Ipfp())
+    Progress: 100%|███████████████████████████████████████████████████████████████████████████████████████████████| Time: 0:00:00
+    0.2780719051126377
+    ```
+    """
+    function connected_information(joined_probability::Array{T}, order::Int; method = Cone(MosekOptimizer())) where T <: Real
+
+        order > ndims(joined_probability) && 
             throw(DomainError("Marginal size cannot be greater than number of dimensions of joined probability"))
-        marginal_size < 2 && 
+        order < 2 && 
             throw(DomainError("Marginal size for connected information cannot be less than 2"))
 
-        entropy1 = maximize_entropy(joined_probability, marginal_size - 1; method).entropy
-        entropy2 = maximize_entropy(joined_probability, marginal_size; method).entropy
+        entropy1 = maximize_entropy(joined_probability, order - 1; method).entropy
+        entropy2 = maximize_entropy(joined_probability, order; method).entropy
         return entropy1 - entropy2
     end
 
-    # More efficient computation when computing multiple connected informations
-    function connected_information(joined_probability::Array{T}, marginal_sizes::Vector{Int}; method = Cone(MosekOptimizer())) where T <: Real
+    """
+    connected_information(joined_probability::Array{T}, order::Int; method = Cone(MosekOptimizer())) where T <: Real
 
-        sort!(marginal_sizes)
+    Computes connected information for given joined probability and multiple `orders`. Optional argument `method` 
+    specifies which method to use for optimization. Default is `Cone()`. Preffered when computing multiple connected
+    informations - more efficient.
+        
+    # Examples
 
-        max_size = marginal_sizes[end]
-        min_size = marginal_sizes[1]
+    ```julia-repl
+    julia> x = [0.25; 0;; 0; 0.25;;; 0; 0.25;; 0.25; 0]
+    2×2×2 Array{Float64, 3}:
+    [:, :, 1] =
+     0.25  0.0
+     0.0   0.25
+    
+    [:, :, 2] =
+     0.0   0.25
+     0.25  0.0
+    
+    julia> connected_information(x, [2, 3]; method = Ipfp())
+    Dict{Int64, Float64} with 2 entries:
+      2 => 0.0
+      3 => 1.0
+    ```
+    """
+    function connected_information(joined_probability::Array{T}, orders::Vector{Int}; method = Cone(MosekOptimizer())) where T <: Real
+
+        sort!(orders)
+
+        max_size = orders[end]
+        min_size = orders[1]
 
         max_size > ndims(joined_probability) && 
             throw(DomainError("Marginal size cannot be greater than number of dimensions of joined probability"))
         min_size < 2 && 
             throw(DomainError("Marginal size for connected information cannot be less than 2"))
 
-        set_marginals = Set([marginal_sizes..., (marginal_sizes.-1)...])
+        set_marginals = Set([orders..., (orders.-1)...])
         dict_entropies = Dict{Int, Float64}()
 
         for m in set_marginals
@@ -89,7 +161,7 @@ module EntropyMaximisation
 
         ret_dict = Dict{Int, Float64}()
 
-        for m in marginal_sizes
+        for m in orders
             entropy1 = dict_entropies[m-1]
             entropy2 = dict_entropies[m]
             ret_dict[m] = entropy1 - entropy2
--- src/utils.jl
+++ src/utils.jl
@@ -7,6 +7,26 @@ function entropy(probability::T) where {T <: Real}
     return -probability * log2(probability)
 end
 
+"""
+distribution_entropy(distribution::Array{T}) where {T <: Real}
+
+Computes entropy of given distribution. All values in distribution have to be non-negative.
+
+# Example
+
+```julia-repl
+julia> x = [0.1 0.4; 0.4 0.1]
+2×2 Matrix{Float64}:
+ 0.1  0.4
+ 0.4  0.1
+
+julia> distribution_entropy(x)
+1.7219280948873623
+
+julia> distribution_entropy([0.25 0.25; 0.25 0.25])
+2.0
+```
+"""
 function distribution_entropy(distribution::Array{T}) where {T <: Real}
     any(x -> x < 0, distribution) && throw(DomainError("Distribution entropy cannot be computed for negative value"))
     return sum(entropy.(distribution))
@@ -15,7 +35,6 @@ end
 function vecs2tuples(vectors::Array{Vector{T}})::Array{Tuple{Vararg{T}}} where {T}
     return [Tuple{Vararg{T}}(v) for v in vectors]
 end
-
 
 function permutations_of_array(arr::Array{Int}, length::Int)::Vector{Vector{Int}}
     if length == 1
@@ -30,6 +49,30 @@ function permutations_of_array(arr::Array{Int}, length::Int)::Vector{Vector{Int}
     return ret
 end
 
+"""
+permutations_of_length(length::Int, dims::Int)::Vector{Tuple} 
+
+Finds all permutations of given size from elements from 1 to `dims` in ascending order.
+
+# Example
+
+```julia-repl
+julia> permutations_of_length(3,4)
+4-element Vector{Tuple}:
+ (1, 2, 3)
+ (1, 2, 4)
+ (1, 3, 4)
+ (2, 3, 4)
+
+julia> permutations_of_length(1,5)
+5-element Vector{Tuple}:
+ (1,)
+ (2,)
+ (3,)
+ (4,)
+ (5,)
+```
+"""
 function permutations_of_length(length::Int, dims::Int)::Vector{Tuple}
     length > dims && throw(DomainError("Length $length cannot be greater than number of dimensions $dims"))
     length < 1 && throw(DomainError("Length $length has to be possitive"))
